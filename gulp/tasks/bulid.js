25812e458a86d1bbb225b08fb1e4063c05d56af6
vinod10 <vinodkumar@kwiqpick.com>
2017-11-20 18:50:39 +0530

    added sidebar for restaurants page

--- gulp/tasks/bulid.js
+++ gulp/tasks/bulid.js
@@ -32,7 +32,7 @@ gulp.task('optimizeImages',['deleteDistFolder'], function () {
 });
 
 gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function () {
-    return gulp.src('./app/index.html')
+    return gulp.src('./app/*.html')
         .pipe(usemin({
             css: [function () {
                 return rev()
